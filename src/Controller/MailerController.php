d4074c0bdc7a5df96d4203ba6c364aad1ff1276c
Cezar Pendarovski <cezarpendarovski@web.de>
2021-03-04 08:29:32 +0000

    Check that sending emails is successful

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class MailerController extends AbstractController
+{
+    /**
+     * @Route("/email")
+     * @param MailerInterface $mailer
+     * @return Response
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
+     */
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('ADGGILRIsupport@cgiar.org')
+            ->to('d.mogaka@cgiar.org')
+            ->cc('cezar.pendarovski@roslin.ed.ac.uk')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+
+        return new Response('Email sent!');
+    }
+}

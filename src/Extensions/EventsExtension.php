1aa9dad1851c14b1020665fd1614fbcd2a3111a5
Johanna Benndorf <johanna.benndorf@gmail.com>
2021-04-29 16:07:35 +0100

    Refactored EventsExtension and added exception handling

--- src/Extensions/EventsExtension.php
+++ src/Extensions/EventsExtension.php
@@ -5,9 +5,10 @@ namespace App\Extensions;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\ContextAwareQueryCollectionExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
- * Class NewEventsExtension
+ * Class EventsExtension
  * @package App\Extensions
  * @see https://api-platform.com/docs/core/extensions/
  * @see https://symfonycasts.com/screencast/api-platform-security/query-extension
@@ -20,29 +21,45 @@ class EventsExtension implements ContextAwareQueryCollectionExtensionInterface
             return;
         }
 
-        //Retrieves the request path (e.g. /animal_events/calving_events)
-        $request_path = $context['request_uri'];
-        //Retrieves the event type's lookup value
-        $event_type = $this->retrieveEventTypeConstantValue($request_path);
+        $requestUri = $context['request_uri'];
+        $pathParameter = $this->retrievePathParameter($requestUri);
 
+        //If the {event_type} parameter is invalid, return a 404 exception.
+        if (!$this->retrieveConstantValue($pathParameter)){
+            throw new NotFoundHttpException(
+                sprintf('Resources for parameter value \'%s\' have not been found. See the API documentation for a full list of available parameters.', $pathParameter)
+            );
+        }
+
+        $constantValue = $this->retrieveConstantValue($pathParameter);
         $rootAlias = $queryBuilder->getRootAliases()[0];
         $queryBuilder
             ->andWhere(sprintf('%s.eventType = :eventType', $rootAlias))
-            ->setParameter('eventType', $event_type)
+            ->setParameter('eventType', $constantValue)
         ;
     }
 
     /**
-     * Retrieves the constant (e.g. EVENT_TYPE_CALVING) associated with a given
-     * request parameter (e.g. calving_events) and returns its value (e.g. 1)
+     * Retrieves the {event_type} path parameter of a given request URI.
      *
-     * @param $request_path
+     * @param $requestUri
      * @return mixed
      */
-    public function retrieveEventTypeConstantValue($request_path){
-        preg_match('/animal_events\/(.*?)_events/', $request_path, $matches);
-        $eventParameter = $matches[1];
-        $eventTypeConstant = strtoupper('EVENT_TYPE_'.$eventParameter);
+    private function retrievePathParameter($requestUri){
+        preg_match('/animal_events\/(.*?_events)/', $requestUri, $matches);
+        return $matches[1];
+    }
+
+    /**
+     * Retrieves the constant (e.g. EVENT_TYPE_CALVING) associated with a given
+     * path parameter (e.g. calving_events) and returns its value (e.g. 1).
+     *
+     * @param $pathParameter
+     * @return mixed
+     */
+    private function retrieveConstantValue($pathParameter){
+        $eventType = preg_replace('/_events/', '', $pathParameter);
+        $eventTypeConstant = strtoupper('EVENT_TYPE_'.$eventType);
         if (defined('\App\Entity\AnimalEvent::'.$eventTypeConstant)){
             return constant('\App\Entity\AnimalEvent::'.$eventTypeConstant);
         }

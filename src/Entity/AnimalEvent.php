03be5c03760c2593a19890e45261476c3d8800fc
Cezar Pendarovski <cezarpendarovski@web.de>
2021-05-06 15:35:02 +0100

    Add MilkingEventListener instead of MilkingEventDataProvider

--- src/Entity/AnimalEvent.php
+++ src/Entity/AnimalEvent.php
@@ -21,6 +21,7 @@ use App\Entity\Traits\{
     OrganisationTrait,
     UpdatedTrait
 };
+use App\EventListener\MilkingEventListener;
 use App\Filter\CountryISOCodeFilter;
 use App\Repository\AnimalEventRepository;
 use Doctrine\ORM\Mapping as ORM;
@@ -142,6 +143,7 @@ use Doctrine\ORM\Mapping as ORM;
  *     CountryISOCodeFilter::class
  * )
  * @ORM\Table(name="core_animal_event", indexes={@ORM\Index(name="animal_id", columns={"animal_id"}), @ORM\Index(name="country_id", columns={"country_id", "region_id", "district_id", "ward_id", "village_id"}), @ORM\Index(name="data_collection_date", columns={"data_collection_date"}), @ORM\Index(name="event_date", columns={"event_date"}), @ORM\Index(name="event_type", columns={"event_type"}), @ORM\Index(name="lactation_id", columns={"lactation_id"}), @ORM\Index(name="org_id", columns={"org_id", "client_id"})})
+ * @ORM\EntityListeners({MilkingEventListener::class})
  * @ORM\Entity(repositoryClass=AnimalEventRepository::class)
  */
 class AnimalEvent

4f0f125111270ed002d49731ff3ffd00f2ab9342
Cezar Pendarovski <cezarpendarovski@web.de>
2021-06-04 10:12:29 +0100

    Adjust code style in Filter files

--- src/Filter/CountryISOCodeFilter.php
+++ src/Filter/CountryISOCodeFilter.php
@@ -1,9 +1,5 @@
 <?php
 
-/**
- * (c) Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
- */
-
 namespace App\Filter;
 
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\AbstractContextAwareFilter;
@@ -12,19 +8,31 @@ use App\Entity\Country;
 use Doctrine\ORM\QueryBuilder;
 
 /**
- * Custom filter that retrieves the id from the table core_country (entity Country) for a given ISO 3166-1 alpha-2 letters code.
+ * CountryISOCodeFilter
+ *
+ * Custom filter that retrieves the id from the table core_country (entity Country)
+ * for a given ISO 3166-1 alpha-2 letters code.
  * The retrieved id can be then used to filter other entities for their property countryId.
+ *
  * @see https://api-platform.com/docs/core/filters/#creating-custom-filters
  * @see https://symfonycasts.com/screencast/api-platform-extending/entity-filter-logic
  * @see https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+ *
+ * @author Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
  */
 class CountryISOCodeFilter extends AbstractContextAwareFilter
 {
     /**
      * @inheritDoc
      */
-    protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
-    {
+    protected function filterProperty(
+        string $property,
+        $value,
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        string $operationName = null
+    ) {
         if ($property !== 'countryCode') {
             return;
         }

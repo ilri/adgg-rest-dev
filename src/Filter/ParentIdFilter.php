7e8bb4fa6b5968b52fcfc22b0581c96db8917373
Mirieri David Mogaka <davemirieri@gmail.com>
2022-11-28 15:01:11 +0300

    changes to add parent id as filter option

---
+++ src/Filter/ParentIdFilter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Filter;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\AbstractContextAwareFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\CountryUnits;
+use Doctrine\ORM\QueryBuilder;
+
+class ParentIdFilter extends AbstractContextAwareFilter
+{
+
+    protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
+    {
+        if ($property !== 'countryunits') {
+            return;
+        }
+
+
+        $countryUnitRepository = $this->managerRegistry->getRepository(CountryUnits::class);
+        $country = $countryUnitRepository->findOneBy(['countryunits' => $value]);
+        $regioncode = $country ? $country->getCode() : 0;
+
+        $alias = $queryBuilder->getRootAliases()[0];
+        $valueParameter = $queryNameGenerator->generateParameterName('parentid');
+        $queryBuilder
+            ->andWhere(sprintf('%s.parentid = :%s', $alias, $valueParameter))
+            ->setParameter($valueParameter, $regioncode)
+        ;
+    }
+
+    public function getDescription(string $resourceClass): array
+    {
+        return [
+            'parentid' => [
+                'name' => 'Parent id',
+                'property' => null,
+                'type' => 'int',
+                'required' => false,
+                'description' => 'use the parent id to filter',
+                'openapi' => [
+                    'allowEmptyValue' => false,
+                ]
+            ]
+        ];
+    }
+}

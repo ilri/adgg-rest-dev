35826e77134899563f5659803058db359c59398e
Mirieri David <Mirieri@users.noreply.github.com>
2024-02-05 23:42:43 +0300

    set listtype to id in master list

---
+++ src/Serializer/Normalizer/MasterListNormalizer.php
@@ -0,0 +1,34 @@
+<?php
+
+// src/Serializer/Normalizer/MasterListNormalizer.php
+
+namespace App\Serializer\Normalizer;
+
+use App\Entity\MasterList;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
+
+class MasterListNormalizer implements ContextAwareNormalizerInterface
+{
+    public function supportsNormalization($data, $format = null, array $context = [])
+    {
+        return $data instanceof MasterList;
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $data = [
+            '@id' => '/api/master_lists/'.$object->getId(),
+            '@type' => 'MasterList',
+            'id' => $object->getId(),
+            'value' => $object->getValue(),
+            'label' => $object->getLabel(),
+            'description' => $object->getDescription(),
+            'createdAt' => $object->getCreatedAt()->format(\DateTime::RFC3339),
+            'createdBy' => $object->getCreatedBy(),
+            'orderBy' => $object->getOrderBy(),
+            'listtype' => intval($object->getListType()->getId()), // Convert listtype to integer
+        ];
+
+        return $data;
+    }
+}

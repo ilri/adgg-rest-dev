6db9f73647827a388b2a8c78e942764ace864fe9
Mirieri David <Mirieri@users.noreply.github.com>
2024-02-29 09:58:21 +0300

    changes to allow the listener to use mob data id to search for animalId

--- src/EventListener/LactationListener.php
+++ src/EventListener/LactationListener.php
@@ -2,12 +2,21 @@
 
 namespace App\EventListener;
 
+use App\Entity\Animal;
 use App\Entity\AnimalEvent;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Query\ResultSetMapping;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 
 class LactationListener
 {
+    private $entityManager;
+
+    public function __construct($entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @ORM\PrePersist()
      *
@@ -20,13 +29,40 @@ class LactationListener
             return;
         }
 
-        $animal = $milkingEvent->getAnimal();
-        $calving = $animal->getLastCalving();
+        $animalId = $this->fetchAnimalId($milkingEvent->getMobAnimalDataId());
 
-        if (!$calving) {
-            return;
+        if ($animalId === null) {
+            throw new \RuntimeException('Animal is null in LactationListener for event with ID: ' . $milkingEvent->getId());
+        }
+
+        // Get the Animal entity by its ID
+        $animalEntity = $this->entityManager->getRepository(Animal::class)->find($animalId);
+
+        if ($animalEntity === null) {
+            throw new \RuntimeException('Animal not found for ID: ' . $animalId);
+        }
+
+        $calving = $animalEntity->getLastCalving();
+
+        if ($calving === null) {
+            // Throw an exception if $calving is null
+            throw new \RuntimeException('Calving is null for animal with ID: ' . $animalId);
         }
 
         $milkingEvent->setLactationId($calving->getId());
+    }
+
+    private function fetchAnimalId($mobAnimalDataId)
+    {
+        $rsm = new ResultSetMapping();
+        $rsm->addScalarResult('animalId', 'animalId');
+
+        $sql = 'SELECT fn_getAnimalId_mob(:mobAnimalDataId) as animalId';
+        $query = $this->entityManager->createNativeQuery($sql, $rsm);
+        $query->setParameter('mobAnimalDataId', $mobAnimalDataId);
+
+        $result = $query->getSingleResult();
+
+        return $result['animalId'];
     }
 }

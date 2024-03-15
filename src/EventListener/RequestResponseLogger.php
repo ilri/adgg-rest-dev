91ac953686ea9d4fd6a6ba91d3786f29ba0a9d25
Mirieri David <Mirieri@users.noreply.github.com>
2024-03-15 15:47:26 +0300

    log all request and responses logger

--- src/EventListener/RequestResponseLogger.php
+++ src/EventListener/RequestResponseLogger.php
@@ -2,32 +2,43 @@
 
 namespace App\EventListener;
 
-use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Security;
 use App\Entity\ApiLogs;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Security\Core\Security;
 
 class RequestResponseLogger
 {
-    private $entityManager;
+    private $requestStack;
     private $security;
+    private $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager, Security $security)
+    public function __construct(RequestStack $requestStack, Security $security, EntityManagerInterface $entityManager)
     {
-        $this->entityManager = $entityManager;
+        $this->requestStack = $requestStack;
         $this->security = $security;
+        $this->entityManager = $entityManager;
     }
 
-    public function onKernelRequest(RequestEvent $event)
+    public function onKernelResponse(ResponseEvent $event)
     {
-        // Capture request data and store it in the log entity
-        $request = $event->getRequest();
-        $log = new ApiLogs();
-        $log->setPayload($request->getContent());
-        $log->setRequestUri($request->getUri());
+        $request = $this->requestStack->getCurrentRequest();
+        $response = $event->getResponse();
 
-        // Get the authenticated user's ID if available
+        // Get request and response data
+        $requestData = $request->getContent();
+        $responseData = $response->getContent();
+
+        // Create and persist a log entity
+        $log = new ApiLogs();
+        $log->setPayload(json_encode([
+            'request' => $requestData,
+            'response' => $responseData,
+        ]));
+        $log->setRequestUri($request->getRequestUri());
+
+        // Set the authenticated user's ID if available
         if ($user = $this->security->getUser()) {
             $log->setUserId($user->getId());
         }
@@ -35,21 +46,11 @@ class RequestResponseLogger
         // Set the request method (action) e.g., GET, POST, PUT, DELETE
         $log->setRequestMethod($request->getMethod());
 
+        // Set the status code from the response
+        $log->setStatusCode($response->getStatusCode());
+
         // Persist the log entity
         $this->entityManager->persist($log);
         $this->entityManager->flush();
-    }
-
-    public function onKernelResponse(ResponseEvent $event)
-    {
-        // Capture response data and update the corresponding log entity
-        $response = $event->getResponse();
-        $log = $this->entityManager->getRepository(ApiLogs::class)->findOneBy(['requestUri' => $event->getRequest()->getUri()]);
-        if ($log instanceof ApiLogs) {
-            $log->setStatusCode($response->getStatusCode());
-            // Set other response data as needed
-
-            $this->entityManager->flush();
-        }
     }
 }

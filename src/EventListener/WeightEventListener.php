918effa921ba87bf4076b14550f246ed18d43aa8
Mirieri David <Mirieri@users.noreply.github.com>
2024-03-17 12:36:28 +0300

    cleaning up code for listener making it more readable

--- src/EventListener/WeightEventListener.php
+++ src/EventListener/WeightEventListener.php
@@ -48,140 +48,52 @@ class WeightEventListener
 
         $validationErrors = [];
 
-        // Call the new getMatureHeartGirthLimitsRecord method
-        $heartGirth = $this->getMatureHeartGirthLimitsRecord($weightEvent);
-
-        // Retrieve Mature Heart Girth limits from parameter list
-        $heartGirthLimits = $this->getParameterListValues('mature_heart_girth_limits');
-
-        // Check if the Heart Girth records meet the limits
-        if (($heartGirth < $heartGirthLimits['min_value'] || $heartGirth >= $heartGirthLimits['max_value']) & $heartGirth !== NULL) {
-            $validationErrors[] = sprintf(
-                'Mature animal Heart Girth should be in the range (%f to %f) for animal: %s',
-                $heartGirthLimits['min_value'],
-                $heartGirthLimits['max_value'],
-                $animalId
-            );
+        try {
+            $this->validateMatureHeartGirth($weightEvent);
+        } catch (\RuntimeException $e) {
+            $validationErrors[] = $e->getMessage();
         }
 
-        // Call the new getMatureHeartGirthLimitsRecord method
-        $weight = $this->getMatureWeightLimitsRecord($weightEvent);
-
-        // Retrieve Mature Heart Girth limits from parameter list
-        $weightLimits = $this->getParameterListValues('mature_weight_limits');
-
-        // Check if the Heart Girth records meet the limits
-        if (($weight < $weightLimits['min_value'] || $weight >= $weightLimits['max_value']) & $weight !== NULL) {
-            $validationErrors[] = sprintf(
-                'Mature animal Weight should be in the range (%f to %f) for animal: %s',
-                $weightLimits['min_value'],
-                $weightLimits['max_value'],
-                $animalId
-            );
+        try {
+            $this->validateMatureWeightLimits($weightEvent);
+        } catch (\RuntimeException $e) {
+            $validationErrors[] = $e->getMessage();
         }
 
-        $bodyConditionScore = $this->getMatureBodyCondScoreRecord($weightEvent);
-
-        // Retrieve Mature Heart Girth limits from parameter list
-        $bodyConditionScoreLimits = $this->getParameterListValues('mature_body_cond_score');
-
-        // Check if the Heart Girth records meet the limits
-        if (($bodyConditionScore < $bodyConditionScoreLimits['min_value'] || $bodyConditionScore >= $bodyConditionScoreLimits['max_value']) & $bodyConditionScore !== NULL) {
-            $validationErrors[] = sprintf(
-                'Mature animal body condition score should be in the range (%f to %f) for animal: %s',
-                $bodyConditionScoreLimits['min_value'],
-                $bodyConditionScoreLimits['max_value'],
-                $animalId
-            );
+        try {
+            $this->validateMatureAnimalBodyConditionScore($weightEvent);
+        } catch (\RuntimeException $e) {
+            $validationErrors[] = $e->getMessage();
         }
 
-        $bodyLength = $this->getMatureBodyLengthRecord($weightEvent);
-
-        // Retrieve Mature body length limits from parameter list
-        $bodyLengthLimits = $this->getParameterListValues('mature_body_length');
-
-        // Check if the Heart Girth records meet the limits
-        if (($bodyLength < $bodyLengthLimits['min_value'] || $bodyLength >= $bodyLengthLimits['max_value']) & $bodyLength !== NULL) {
-            $validationErrors[] = sprintf(
-                'Mature animal body length should be in the range (%f to %f) for animal: %s',
-                $bodyLengthLimits['min_value'],
-                $bodyLengthLimits['max_value'],
-                $animalId
-            );
+        try {
+            $this->validateMatureAnimalBodyLength($weightEvent);
+        } catch (\RuntimeException $e) {
+            $validationErrors[] = $e->getMessage();
         }
 
-        $registrationAnimalType = (int)$this->fetchAnimalType($weightEvent->getMobAnimalDataId());
-
-        $calfBodyLength = $this->getMatureBodyLengthRecord($weightEvent);
-
-        // Retrieve Body Length limits from parameter list
-        $calfBodyLengthLimits = $this->getParameterListValues('calf_body_cond_score');
-
-        if (($calfBodyLength < $calfBodyLengthLimits['min_value'] ||
-                $calfBodyLength >= $calfBodyLengthLimits['max_value']) &&
-            $calfBodyLength !== NULL &&
-            in_array($registrationAnimalType, [3, 4, 9, 10])
-        ) {
-            $validationErrors[] = sprintf(
-                'Calf body length should be in the range (%f to %f) for animal: %s',
-                $calfBodyLengthLimits['min_value'],
-                $calfBodyLengthLimits['max_value'],
-                $animalId
-            );
+        try {
+            $this->validateCalfBodyConditionScore($weightEvent);
+        } catch (\RuntimeException $e) {
+            $validationErrors[] = $e->getMessage();
         }
 
-        $calfWeight = $this->getMatureWeightLimitsRecord($weightEvent);
-
-        // Retrieve Calf weight limits from parameter list
-        $calfWeightLimits = $this->getParameterListValues('calf_weight_limits');
-
-        if (($calfWeight < $calfWeightLimits['min_value'] ||
-                $calfWeight >= $calfWeightLimits['max_value']) &&
-            $calfWeight !== NULL &&
-            in_array($registrationAnimalType, [3, 4, 9, 10])
-        ) {
-            $validationErrors[] = sprintf(
-                'Calf weight should be in the range (%f to %f) for animal: %s',
-                $calfWeightLimits['min_value'],
-                $calfWeightLimits['max_value'],
-                $animalId
-            );
+        try {
+            $this->validateCalfWeightLimits($weightEvent);
+        } catch (\RuntimeException $e) {
+            $validationErrors[] = $e->getMessage();
         }
 
-        $calfHeartGirth = $this->getMatureHeartGirthLimitsRecord($weightEvent);
-
-        // Retrieve Calf Heart Girth limits from parameter list
-        $calfHeartGirthLimits = $this->getParameterListValues('calf_heart_girth_limits');
-
-        if (($calfHeartGirth < $calfHeartGirthLimits['min_value'] ||
-                $calfHeartGirth >= $calfHeartGirthLimits['max_value']) &&
-            $calfHeartGirth !== NULL &&
-            in_array($registrationAnimalType, [3, 4, 9, 10])
-        ) {
-            $validationErrors[] = sprintf(
-                'Calf Heart Girth should be in the range (%f to %f) for animal: %s',
-                $calfHeartGirthLimits['min_value'],
-                $calfHeartGirthLimits['max_value'],
-                $animalId
-            );
+        try {
+            $this->validateCalfHeartGirth($weightEvent);
+        } catch (\RuntimeException $e) {
+            $validationErrors[] = $e->getMessage();
         }
 
-        $calfBodyLength = $this->getMatureBodyLengthRecord($weightEvent);
-
-        // Retrieve Calf Heart Girth limits from parameter list
-        $calfBodyLengthLimits = $this->getParameterListValues('calf_body_length');
-
-        if (($calfBodyLength < $calfBodyLengthLimits['min_value'] ||
-                $calfBodyLength >= $calfBodyLengthLimits['max_value']) &&
-            $calfBodyLength !== NULL &&
-            in_array($registrationAnimalType, [3, 4, 9, 10])
-        ) {
-            $validationErrors[] = sprintf(
-                'Calf Body Length should be in the range (%f to %f) for animal: %s',
-                $calfBodyLengthLimits['min_value'],
-                $calfBodyLengthLimits['max_value'],
-                $animalId
-            );
+        try {
+            $this->validateCalfBodyLength($weightEvent);
+        } catch (\RuntimeException $e) {
+            $validationErrors[] = $e->getMessage();
         }
 
         // Check for validation errors and throw a single exception if any
@@ -257,6 +169,181 @@ class WeightEventListener
     {
         $additionalAttributes = $weightEvent->getAdditionalAttributes();
         return $additionalAttributes['138'] ?? NULL;
+    }
+
+    private function validateMatureHeartGirth(AnimalEvent $weightEvent): float
+    {
+        // Call the new getMatureHeartGirthLimitsRecord method
+        $heartGirth = $this->getMatureHeartGirthLimitsRecord($weightEvent);
+
+        // Retrieve Mature Heart Girth limits from parameter list
+        $heartGirthLimits = $this->getParameterListValues('mature_heart_girth_limits');
+
+        // Check if the Heart Girth records meet the limits
+        if (($heartGirth < $heartGirthLimits['min_value'] || $heartGirth >= $heartGirthLimits['max_value']) & $heartGirth !== NULL) {
+            throw new \RuntimeException(sprintf(
+                'Mature animal Heart Girth should be in the range (%f to %f) for animal: %s',
+                $heartGirthLimits['min_value'],
+                $heartGirthLimits['max_value'],
+                $weightEvent->getMobAnimalDataId()
+            ));
+        }
+        return $heartGirth;
+    }
+
+    private function validateMatureWeightLimits(AnimalEvent $weightEvent): float
+    {
+
+        // Call the new getMatureWeightLimitsRecord method
+        $weight = $this->getMatureWeightLimitsRecord($weightEvent);
+
+        // Retrieve Mature Animal weight limits from parameter list
+        $weightLimits = $this->getParameterListValues('mature_weight_limits');
+
+        // Check if the Heart Girth records meet the limits
+        if (($weight < $weightLimits['min_value'] || $weight >= $weightLimits['max_value']) & $weight !== NULL) {
+            throw new \RuntimeException(sprintf(
+                'Mature animal Weight should be in the range (%f to %f) for animal: %s',
+                $weightLimits['min_value'],
+                $weightLimits['max_value'],
+                $weightEvent->getMobAnimalDataId()
+            ));
+        }
+        return $weight;
+
+    }
+
+    private function validateMatureAnimalBodyConditionScore(AnimalEvent $weightEvent): float
+    {
+        $bodyConditionScore = $this->getMatureBodyCondScoreRecord($weightEvent);
+
+        // Retrieve Mature Heart Girth limits from parameter list
+        $bodyConditionScoreLimits = $this->getParameterListValues('mature_body_cond_score');
+
+        // Check if the Heart Girth records meet the limits
+        if (($bodyConditionScore < $bodyConditionScoreLimits['min_value'] || $bodyConditionScore >= $bodyConditionScoreLimits['max_value']) & $bodyConditionScore !== NULL) {
+            throw new \RuntimeException(sprintf(
+                'Mature animal body condition score should be in the range (%f to %f) for animal: %s',
+                $bodyConditionScoreLimits['min_value'],
+                $bodyConditionScoreLimits['max_value'],
+                $weightEvent->getMobAnimalDataId()
+            ));
+        }
+        return $bodyConditionScore;
+
+    }
+
+    private function validateMatureAnimalBodyLength(AnimalEvent $weightEvent): float
+    {
+        $bodyLength = $this->getMatureBodyLengthRecord($weightEvent);
+
+        // Retrieve Mature body length limits from parameter list
+        $bodyLengthLimits = $this->getParameterListValues('mature_body_length');
+
+        // Check if the Heart Girth records meet the limits
+        if (($bodyLength < $bodyLengthLimits['min_value'] || $bodyLength >= $bodyLengthLimits['max_value']) & $bodyLength !== NULL) {
+            throw new \RuntimeException(sprintf(
+                'Mature animal body length should be in the range (%f to %f) for animal: %s',
+                $bodyLengthLimits['min_value'],
+                $bodyLengthLimits['max_value'],
+                $weightEvent->getMobAnimalDataId()
+            ));
+        }
+        return $bodyLength;
+    }
+    private function validateCalfHeartGirth(AnimalEvent $weightEvent): float
+    {
+        $registrationAnimalType = (int)$this->fetchAnimalType($weightEvent->getMobAnimalDataId());
+
+        $calfHeartGirth = $this->getMatureHeartGirthLimitsRecord($weightEvent);
+
+        // Retrieve Calf Heart Girth limits from parameter list
+        $calfHeartGirthLimits = $this->getParameterListValues('calf_heart_girth_limits');
+
+        if (($calfHeartGirth < $calfHeartGirthLimits['min_value'] ||
+                $calfHeartGirth >= $calfHeartGirthLimits['max_value']) &&
+            $calfHeartGirth !== NULL &&
+            in_array($registrationAnimalType, [3, 4, 9, 10])
+        ) {
+            throw new \RuntimeException(sprintf(
+                'Calf Heart Girth should be in the range (%f to %f) for animal: %s',
+                $calfHeartGirthLimits['min_value'],
+                $calfHeartGirthLimits['max_value'],
+                $weightEvent->getMobAnimalDataId()
+            ));
+        }
+        return $calfHeartGirth;
+    }
+
+    private function validateCalfWeightLimits(AnimalEvent $weightEvent): float
+    {
+        $registrationAnimalType = (int)$this->fetchAnimalType($weightEvent->getMobAnimalDataId());
+
+        $calfWeight = $this->getMatureWeightLimitsRecord($weightEvent);
+
+        // Retrieve Calf weight limits from parameter list
+        $calfWeightLimits = $this->getParameterListValues('calf_weight_limits');
+
+        if (($calfWeight < $calfWeightLimits['min_value'] ||
+                $calfWeight >= $calfWeightLimits['max_value']) &&
+            $calfWeight !== NULL &&
+            in_array($registrationAnimalType, [3, 4, 9, 10])
+        ) {
+            throw new \RuntimeException(sprintf(
+                'Calf weight should be in the range (%f to %f) for animal: %s',
+                $calfWeightLimits['min_value'],
+                $calfWeightLimits['max_value'],
+                $weightEvent->getMobAnimalDataId()
+            ));
+        }
+        return $calfWeight;
+    }
+
+    private function validateCalfBodyConditionScore(AnimalEvent $weightEvent): float
+    {
+        $registrationAnimalType = (int)$this->fetchAnimalType($weightEvent->getMobAnimalDataId());
+
+        $calfBodyLength = $this->getMatureBodyLengthRecord($weightEvent);
+
+        // Retrieve Body Length limits from parameter list
+        $calfBodyLengthLimits = $this->getParameterListValues('calf_body_cond_score');
+
+        if (($calfBodyLength < $calfBodyLengthLimits['min_value'] ||
+                $calfBodyLength >= $calfBodyLengthLimits['max_value']) &&
+            $calfBodyLength !== NULL &&
+            in_array($registrationAnimalType, [3, 4, 9, 10])
+        ) {
+            throw new \RuntimeException(sprintf(
+                'Calf body length should be in the range (%f to %f) for animal: %s',
+                $calfBodyLengthLimits['min_value'],
+                $calfBodyLengthLimits['max_value'],
+                $weightEvent->getMobAnimalDataId()
+            ));
+        }
+        return $calfBodyLength;
+    }
+
+    private function validateCalfBodyLength(AnimalEvent $weightEvent): float
+    {
+        $registrationAnimalType = (int)$this->fetchAnimalType($weightEvent->getMobAnimalDataId());
+        $calfBodyLength = $this->getMatureBodyLengthRecord($weightEvent);
+
+        // Retrieve Calf Heart Girth limits from parameter list
+        $calfBodyLengthLimits = $this->getParameterListValues('calf_body_length');
+
+        if (($calfBodyLength < $calfBodyLengthLimits['min_value'] ||
+                $calfBodyLength >= $calfBodyLengthLimits['max_value']) &&
+            $calfBodyLength !== NULL &&
+            in_array($registrationAnimalType, [3, 4, 9, 10])
+        ) {
+            throw new \RuntimeException(sprintf(
+                'Calf Body Length should be in the range (%f to %f) for animal: %s',
+                $calfBodyLengthLimits['min_value'],
+                $calfBodyLengthLimits['max_value'],
+                $weightEvent->getMobAnimalDataId()
+            ));
+        }
+        return $calfBodyLength;
     }
 
 }

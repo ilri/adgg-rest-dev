6a1e9851b0f769ed2d0f258c0ca0291f8e05cd99
Mirieri David <Mirieri@users.noreply.github.com>
2024-03-21 15:46:27 +0300

    add region id and district id as filter for farm metadata bug fix

--- src/Filter/FarmMetadataFilter.php
+++ src/Filter/FarmMetadataFilter.php
@@ -5,34 +5,30 @@ namespace App\Filter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\AbstractContextAwareFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use App\Entity\Farm;
-use App\Entity\FarmMetadata;
 use Doctrine\ORM\QueryBuilder;
 
+/**
+ * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+ */
 class FarmMetadataFilter extends AbstractContextAwareFilter
 {
     /**
      * @param string $property
-     * @param $value
+     * @param mixed $value
      * @param QueryBuilder $queryBuilder
      * @param QueryNameGeneratorInterface $queryNameGenerator
      * @param string $resourceClass
      * @param string|null $operationName
      * @return void
      */
-    protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
+    protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, ?string $operationName = null): void
     {
         if (!in_array($property, ['regionId', 'districtId'])) {
             return;
         }
 
-        if ($property === 'regionId') {
-            $property = 'regionId';
-        } elseif ($property === 'districtId') {
-            $property = 'districtId';
-        }
-
         $rootAlias = $queryBuilder->getRootAliases()[0];
-        $queryBuilder->innerJoin(Farm::class, 'farm', 'WITH', "$rootAlias.farm = farm.id");
+        $queryBuilder->innerJoin($rootAlias . '.farm', 'farm');
 
         $parameterName = $queryNameGenerator->generateParameterName($property);
         $queryBuilder->andWhere("farm.$property = :$parameterName")
